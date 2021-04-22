ebfaf84eec57230258fce5e926765154e756c7ce
ervin-mitra <ervin.mitra@gopuff.com>
2021-04-22 16:45:06 -0700

    pass as props

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -7,10 +7,15 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <Header />
+        <Header subtitle="Providing Houses All Over The Milky Way"/>
       </div>
     );
   }
 }
 
 export default App;
+
+// class component supports state and lifecycle methods
+// as a rule of thumb:
+// if you want to just render some JSX, then use function components
+// use class components for everything else 
